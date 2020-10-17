39191aec4db76f32296f9cda353dc5b0968b1d09
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2020-10-16 21:29:24 -0400

    check for parents that are installed, not those in graph
    Also, install/remove on parent first, and simplify by getting rid of
    transactional component set.

--- graph.rb
+++ graph.rb
@@ -5,7 +5,7 @@ class Graph
   
   def initialize(logger)
     @edges = {}
-    @components = ComponentSet.new(@edges)
+    @components = Set.new
     @logger = logger
   end
 
@@ -14,74 +14,34 @@ class Graph
   end
 
   def install(node, depth = 0)
-    if @edges.has_key?(node)
-      @edges[node].each { |n| install(n, depth + 1) }
-    end
     unless @components.include?(node)
       @components << node 
       logger.log("Installing #{node}.", depth)
     else
       logger.log("#{node} is already installed.", depth)
     end
-    @components.commit!
+    if @edges.has_key?(node)
+      @edges[node].each { |n| install(n, depth + 1) }
+    end
   end
 
   def remove(node, depth = 0)
-    if @edges.has_key?(node)
-      @edges[node].each { |n| remove(n, depth + 1) }
-    end
     if @components.include?(node)
-      if @edges.values.any? { |deps| deps.include?(node) }
-        logger.log("#{node} is still needed.", depth)
+      if (parents = @components.select { |p| @edges[p] && @edges[p].include?(node) }).any?
+        logger.log("#{node} is still needed by #{parents}.", depth)
       else
         @components.delete(node)
         logger.log("Removing #{node}.", depth)
       end
     end
-    @components.commit!
+    if @edges.has_key?(node)
+      @edges[node].each { |n| remove(n, depth + 1) }
+    end
   end
 
   def list
     @components.each do |component|
       logger.log(component, 1)
-    end
-  end
-
-  class ComponentSet
-    def initialize(dependencies)
-      @set = Set.new
-      @modified_set = Set.new
-      @dependencies = dependencies
-    end
-
-    def <<(component)
-      @modified_set << component
-    end
-
-    def delete(component)
-      @modified_set.delete(component)
-    end
-
-    def include?(component)
-      @modified_set.include? component
-    end
-
-    def each(&block)
-      @set.each &block
-    end
-
-    def commit!
-      removed = @set - @modified_set
-      new_set = @set
-      if removed.any?
-        removed.reject! { |component| @dependencies.values.any? { |deps| deps.include?(component) } }
-        new_set = @set - removed
-      end
-      added = @modified_set - @set
-      if added.any?
-        new_set = new_set + added
-      end
-      @set = new_set
     end
   end
 end
